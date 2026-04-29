3699f73fc58911e2ab2a45232616f28038cf9fbf
Lavanya <lavanya150598@gmail.com>
2026-04-29 15:57:03 +0530

    adding content

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("🚀 Hello Lavanya DevOps - Webhook Working Fixed now for poll SCM!");
+    res.end("🚀 Hello Lavanya DevOps - Webhook Working Fixed now for poll SCM welcome to devopss!");
 });
 
 server.listen(3000, () => {
